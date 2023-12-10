0148e9e3850284a2f3939d30f0e44320b1cf3570
IsabellaCaputi <110950027+IsabellaCaputi@users.noreply.github.com>
2023-12-10 15:35:19 -0500

    Factura

--- src/uniandes/dpoo/proyecto1/interfaz/Factura.java
+++ src/uniandes/dpoo/proyecto1/interfaz/Factura.java
@@ -4,8 +4,6 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
-import org.apache.pdfbox.pdmodel.PDPageTree;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import java.io.IOException;
@@ -15,47 +13,33 @@ import java.util.Date;
 import java.io.IOException;
 
 public class Factura {
-	public static void generarFactura(String nombreCliente, String nombreVehiculo, String firmaAdministrador, String nombreArchivo) {
+	public static void generarFactura(String nombreCliente, String placaVehiculo) {
         try (PDDocument document = new PDDocument()) {
             PDPage page = new PDPage();
             document.addPage(page);
-
             PDPageContentStream contentStream = new PDPageContentStream(document, page, AppendMode.APPEND, true);
-            //contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
             contentStream.setLeading(14.5f);
-
-            // Contenido de la factura (puedes personalizarlo según tus necesidades)
             contentStream.beginText();
             contentStream.newLineAtOffset(25, 700);
             contentStream.showText("Factura");
             contentStream.newLine();
-
             contentStream.showText("Cliente: " + nombreCliente);
             contentStream.newLine();
-
-            contentStream.showText("Vehículo: " + nombreVehiculo);
+            contentStream.showText("Vehículo: " + placaVehiculo);
             contentStream.newLine();
-
             contentStream.showText("Fecha: " + new Date());
             contentStream.newLine();
-
             contentStream.endText();
-
             // Agregar firma del administrador como imagen
-            PDImageXObject pdImage = PDImageXObject.createFromFile("ruta_de_la_imagen_firma_admin.png", document);
+            PDImageXObject pdImage = PDImageXObject.createFromFile("data/firmaAdmin.png", document);
             contentStream.drawImage(pdImage, 25, 500, pdImage.getWidth() / 4, pdImage.getHeight() / 4);
-
             contentStream.close();
-
             // Guardar el documento como archivo PDF
-            document.save(nombreArchivo);
-            System.out.println("Factura generada correctamente en: " + nombreArchivo);
+            document.save("data/Factura");
+            System.out.println("Factura generada correctamente en: " + "data/Factura");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        generarFactura("Juan Pérez", "Automóvil Sedán", "ruta_de_la_firma_admin.png", "factura.pdf");
-    }
 }
